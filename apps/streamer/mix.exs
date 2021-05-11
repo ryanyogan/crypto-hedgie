7b1b50381bf39667c7d0168723cd1ad9f2d3335e
ryan <ryanyogan@gmail.com>
2021-05-11 10:18:23 -0500

    wired up event stream, marshal input into a %TradeEvent{}

--- apps/streamer/mix.exs
+++ apps/streamer/mix.exs
@@ -15,7 +15,6 @@ defmodule Streamer.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -23,9 +22,9 @@ defmodule Streamer.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:jason, "~> 1.2"},
       {:websockex, "~> 0.4.2"}
     ]
   end
